cf4235e36eccc00c1c15eb5dd5a47ff773a25966
VARD0V <dimadima09876@gmail.com>
2025-05-13 18:54:50 +0700

    CSS+HTML главная страница - статьи

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,44 +1,32 @@
 @extends('layouts.layout')
 @section('title', 'LibraryAI')
+
 @section('content')
-    <h1>Список статей</h1>
-
     @can('create', App\Models\Article::class)
-        <a href="{{ route('articles.create') }}">Создать новую статью</a>
+        <div class="create-link">
+            <a href="{{ route('articles.create') }}">Создать новую статью</a>
+        </div>
     @endcan
-
-    <table class="table">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Заголовок</th>
-            <th>Категория</th>
-            <th>Действия</th>
-        </tr>
-        </thead>
-        <tbody>
+    <div class="articles-container">
         @foreach ($articles as $article)
-            <tr>
-                <td>{{ $article->id }}</td>
-                <td>{{ $article->title }}</td>
-                <td>{{ $article->articleCategory->name }}</td>
-                <td>
-                    <a href="{{ route('articles.show', $article) }}">Просмотр</a>
+            <a href="{{ route('articles.show', $article) }}" class="article-link">
+                <div class="article-card-horizontal">
+                    <img src="{{ $article->preview ? asset('storage/' . $article->preview) : asset('assets/images/main-page2.png') }}"
+                         alt="Preview" class="article-image-horizontal">
 
-                    @can('update', $article)
-                        <a href="{{ route('articles.edit', $article) }}">Редактировать</a>
-                    @endcan
-
-                    @can('delete', $article)
-                        <form action="{{ route('articles.destroy', $article) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Удалить</button>
-                        </form>
-                    @endcan
-                </td>
-            </tr>
+                    <div class="article-details">
+                        <h2 class="article-title">{{ $article->title }}</h2>
+                        <p class="article-description">{{ $article->description }}</p>
+                        <p class="article-text">
+                            {{Str::words(strip_tags($article->text), 100, '...') }}
+                        </p>
+                        <div class="article-meta-horizontal">
+                            <span>{{ $article->articleCategory->name }}</span>
+                            <span>{{ $article->author->login }}</span>
+                        </div>
+                    </div>
+                </div>
+            </a>
         @endforeach
-        </tbody>
-    </table>
+    </div>
 @endsection
